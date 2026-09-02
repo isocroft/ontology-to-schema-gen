0ee8cb9922d37574d4fdcf67c7a893538ba3cd8f
Ifeora Okechukwu Patrick <isocroft@users.noreply.github.com>
2026-09-02 01:44:42 +0100

    impl(helpers): added `CapitalizeWordsManual(...)` function for string capitalization
    Added a function to capitalize the first letter of each word in a string, handling Unicode characters.

--- ontology/schema_naming_helpers.go
+++ ontology/schema_naming_helpers.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var nonIdentifier = regexp.MustCompile(`[^a-zA-Z0-9_]+`)
+
+func CapitalizeWordsManual(input string) string {
+	words := strings.Fields(input) // @INFO: Splits by whitespace
+	for i, word := range words {
+		if word == "" {
+			continue
+		}
+		// @HINT: Decode the first rune (safely handle Unicode characters)
+		r, size := utf8.DecodeRuneInString(word)
+		if r == utf8.RuneError {
+			continue
+		}
+		// @HINT: Capitalize the first rune and append the rest of the word
+		words[i] = string(unicode.ToUpper(r)) + word[size:]
+	}
+	return strings.Join(words, " ")
+}
 
 func NormalizeIdentifier(value string) string {
 	value = strings.TrimSpace(value)
