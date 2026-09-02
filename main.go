c943bc9e2e73c71acf090ebc3b1295fee439e504
Ifeora Okechukwu Patrick <isocroft@users.noreply.github.com>
2026-09-02 02:37:38 +0100

    impl(entry-point): initialized main function with error handling

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/isocroft/ontology-to-schema-gen/cmd"
+)
+
+func main() {
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error |> \r\n", err)
+		os.Exit(1)
+	}
+}
